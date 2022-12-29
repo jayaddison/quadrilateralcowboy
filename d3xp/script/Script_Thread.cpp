360bc5576aff3e5bc85ff55c831906f845a973b3
Daniel Gibson <metalcaedes@gmail.com>
2022-12-29 05:38:13 +0100

    Fix -Wformat-security warnings - thanks James Addison!
    This is based on https://github.com/dhewm/dhewm3/pull/500
    by https://github.com/jayaddison
    
    See also https://github.com/blendogames/quadrilateralcowboy/pull/4
    
    (cherry picked from commit 279a40a981594b77aa57e44615c640a71f5ca101)

--- d3xp/script/Script_Thread.cpp
+++ d3xp/script/Script_Thread.cpp
@@ -884,7 +884,7 @@ void idThread::Error( const char *fmt, ... ) const {
 	vsprintf( text, fmt, argptr );
 	va_end( argptr );
 
-	interpreter.Error( text );
+	interpreter.Error( "%s", text );
 }
 
 /*
@@ -900,7 +900,7 @@ void idThread::Warning( const char *fmt, ... ) const {
 	vsprintf( text, fmt, argptr );
 	va_end( argptr );
 
-	interpreter.Warning( text );
+	interpreter.Warning( "%s", text );
 }
 
 /*
